c32acf76015bf008ab213fbdd2746cd28f4e9b55
nailik-git <k.smeets@proton.me>
2025-10-05 11:26:21 +0200

    change sponge and squeeze only call memmove once; optimize keccak

--- sha3.c
+++ sha3.c
@@ -14,144 +14,89 @@ static inline int __sha3_mod(int num, int div) {
   return r;
 }
 
-static inline void __sha3_A_from_string(uint64_t* S, uint64_t A[5][5]) {
-  for(int j = 0; j < 5; j++) {
-    for(int i = 0; i < 5; i++) {
-      A[i][j] = S[5 * j + i];
-    }
-  }
-}
+#define SHA3_C(x) (S[x] ^ S[x + 5] ^ S[x + 10] ^ S[x + 15] ^ S[x + 20])
 
-static inline void __sha3_string_from_A(uint64_t A[5][5], uint64_t* S) {
-  for(int j = 0; j < 5; j++) {
-    for(int i = 0; i < 5; i++) {
-      S[5 * j + i] = A[i][j];
-    }
-  }
-}
 
-#define SHA3_C(x) (A[x][0] ^ A[x][1] ^ A[x][2] ^ A[x][3] ^ A[x][4])
+static const int __sha3_rot[] = {
+	  1,   3,   6,  10,
+	 15,  21,  28,  36,
+	 45,  55,  66,  78,
+	 91, 105, 120, 136,
+	153, 171, 190, 210,
+	231, 253, 276, 300
+};
 
-#define SHA3_D(x) (SHA3_C(__sha3_mod(x - 1, 5)) ^ __builtin_rotateleft64(SHA3_C((x + 1) % 5), 1))
+static const int __sha3_xy[][2] = {
+  {1, 0}, {0, 2}, {2, 1}, {1, 2},
+  {2, 3}, {3, 3}, {3, 0}, {0, 1},
+  {1, 3}, {3, 1}, {1, 4}, {4, 4},
+  {4, 0}, {0, 3}, {3, 4}, {4, 3},
+  {3, 2}, {2, 2}, {2, 0}, {0, 4},
+  {4, 2}, {2, 4}, {4, 1}, {1, 1}
+};
 
-static inline void* __sha3_theta(uint64_t A[5][5]) {
-  static uint64_t _A[5][5] = {0};
+static const int __sha3_pi[25] = {
+  0, 3, 1, 4, 2,
+  1, 4, 2, 0, 3,
+  2, 0, 3, 1, 4,
+  3, 1, 4, 2, 0,
+  4, 2, 0, 3, 1
+};
 
+static const int __sha3_mod5[] = {
+  4, 0, 1, 2, 3, 4, 0, 1
+};
+
+#define SHA3_D(x) (SHA3_C(__sha3_mod5[x]) ^ __builtin_rotateleft64(SHA3_C(__sha3_mod5[x + 2]), 1))
+
+static const uint64_t __sha3_rc[] = {
+  0x0000000000000001, 0x0000000000008082, 0x800000000000808a, 0x8000000080008000,
+  0x000000000000808b, 0x0000000080000001, 0x8000000080008081, 0x8000000000008009,
+  0x000000000000008a, 0x0000000000000088, 0x0000000080008009, 0x000000008000000a,
+  0x000000008000808b, 0x800000000000008b, 0x8000000000008089, 0x8000000000008003,
+  0x8000000000008002, 0x8000000000000080, 0x000000000000800a, 0x800000008000000a,
+  0x8000000080008081, 0x8000000000008080, 0x0000000080000001, 0x8000000080008008
+};
+
+static inline void __sha3_rnd(uint64_t* S, int i_r) {
+  static uint64_t _S[25] = {0};
+
+  // theta
   for(int x = 0; x < 5; x++) {
     for(int y = 0; y < 5; y++) {
-      _A[x][y] = A[x][y] ^ SHA3_D(x);
+      _S[x + 5 * y] = S[x + 5 * y] ^ SHA3_D(x);
     }
   }
 
-  for(int i = 0; i < 5; i++) {
-    for(int j = 0; j < 5; j++) {
-      A[i][j] = _A[i][j];
-    }
-  }
-
-  return A;
-}
-
-static inline void* __sha3_rho(uint64_t A[5][5]) {
-  static uint64_t _A[5][5] = {0};
-
-  _A[0][0] = A[0][0];
-  int x = 1;
-  int y = 0;
+  // rho
+  S[0] = _S[0];
   for(int t = 0; t < 24; t++) {
-    _A[x][y] = __builtin_rotateleft64(A[x][y], (t + 1) * (t + 2) / 2);
-    int tmpx = x;
-    int tmpy = y;
-    x = y;
-    y = (2 * tmpx + 3 * tmpy) % 5;
+    const int x = __sha3_xy[t][0];
+    const int y = __sha3_xy[t][1];
+    _S[x + 5 * y] = __builtin_rotateleft64(_S[x + 5 * y], __sha3_rot[t]);
   }
 
-  for(int i = 0; i < 5; i++) {
-    for(int j = 0; j < 5; j++) {
-      A[i][j] = _A[i][j];
-    }
-  }
-
-  return A;
-}
-
-static inline void* __sha3_pi(uint64_t A[5][5]) {
-  static uint64_t _A[5][5] = {0};
-
+  // pi & chi
   for(int x = 0; x < 5; x++) {
-    for(int y = 0; y < 5; y ++) {
-      _A[x][y] = A[(x + 3 * y) % 5][x];
+    for(int y = 0; y < 5; y++) {
+      const int m1 = __sha3_mod5[x + 2];
+      const int m2 = __sha3_mod5[x + 3];
+
+      const int pi1 = __sha3_pi[x * 5 + y];
+      const int pi2 = __sha3_pi[m1 * 5 + y];
+      const int pi3 = __sha3_pi[m2 * 5 + y];
+      S[x + 5 * y] = _S[pi1 + 5 * x] ^ ((_S[pi2 + 5 * m1] ^ (uint64_t) -1) & _S[pi3 + 5 * m2]);
     }
   }
 
-  for(int i = 0; i < 5; i++) {
-    for(int j = 0; j < 5; j++) {
-      A[i][j] = _A[i][j];
-    }
-  }
-
-  return A;
+  // iota
+  S[0] ^= __sha3_rc[i_r];
 }
-
-static inline void* __sha3_chi(uint64_t A[5][5]) {
-  static uint64_t _A[5][5] = {0};
-
-  for(int x = 0; x < 5; x++) {
-    for(int y = 0; y < 5; y ++) {
-      _A[x][y] = A[x][y] ^ ((A[(x + 1) % 5][y] ^ (uint64_t) -1) & A[(x + 2) % 5][y]);
-    }
-  }
-
-  for(int i = 0; i < 5; i++) {
-    for(int j = 0; j < 5; j++) {
-      A[i][j] = _A[i][j];
-    }
-  }
-
-  return A;
-}
-
-static inline uint64_t __sha3_rc(uint64_t t) {
-  uint64_t R = 1;
-  const uint64_t bitmask = 1 << 8;
-
-  for(size_t i = 1; i <= t % 255; i++) {
-    R <<= 1;
-    R ^= (R & bitmask) >> 8;
-    R ^= (R & bitmask) >> 4;
-    R ^= (R & bitmask) >> 3;
-    R ^= (R & bitmask) >> 2;
-  }
-
-  return R & 1;
-}
-
-static inline void* __sha3_iota(uint64_t A[5][5], int i_r) {
-  uint64_t RC = 0;
-
-  for(int j = 0; j <= SHA3_L; j++) {
-    int shift = (1 << j) - 1;
-
-    RC |= __sha3_rc(j + 7 * i_r) << shift;
-  }
-
-  A[0][0] ^= RC;
-
-  return A;
-}
-
-#define SHA3_RND(A, i_r) __sha3_iota(__sha3_chi(__sha3_pi(__sha3_rho(__sha3_theta(A)))), i_r)
 
 void __sha3_keccak(uint64_t* S) {
-  uint64_t A[5][5] = {0};
-
-  __sha3_A_from_string(S, A);
-
   for(int i = 12 + 2 * SHA3_L - SHA3_N_R; i < (12 + 2 * SHA3_L); i++) {
-    SHA3_RND(A, i);
+    __sha3_rnd(S, i);
   }
-
-  __sha3_string_from_A(A, S);
 }
 
 void __sha3_append(__sha3_da* da, const uint8_t item) {
@@ -223,8 +168,8 @@ void sha3_deinit(sha3* sha3) {
   sha3->hash = NULL;
 }
 
-void __sha3_sponge(sha3 sha3, const int r) {
-  int p_idx = 0;
+void __sha3_sponge(sha3 sha3, const int i, const int r) {
+  int p_idx = i * r / 8;
   for(int j = 0; j < r / 64; j++) {
     for(int k = 0; k < 8; k++) {
       sha3.S[j] ^= ((uint64_t) sha3.buf->items[p_idx + k]) << (8 * k);
@@ -232,17 +177,6 @@ void __sha3_sponge(sha3 sha3, const int r) {
     p_idx += 8;
   }
   __sha3_keccak(sha3.S);
-}
-
-void __sha3_pad(__sha3_da* da, const int x) {
-  const int bit_len = da->count * 8 - 5;
-  const int j = __sha3_mod(-bit_len - 2, x);
-  const int p_len = (bit_len + j + 2) / 8;
-
-  if(da->capacity < p_len) da->items = realloc(da->items, (da->capacity *= 2));
-
-  da->items[p_len - 1] |= 0b10000000;
-  da->count = p_len;
 }
 
 
@@ -254,12 +188,13 @@ void sha3_sponge(sha3* sha3, const void* M, const size_t size) {
   __sha3_append_buf(sha3->buf, M, size);
 
   // if long enough, perform sponging
-  while(sha3->buf->count >= r / 8) {
-    __sha3_sponge(*sha3, r);
-
-    // keep rest
-    memmove(sha3->buf->items, sha3->buf->items + r / 8, (sha3->buf->count -= r / 8));
+  int i;
+  for(i = 0; (i + 1) * r / 8 <= sha3->buf->count; i++) {
+    __sha3_sponge(*sha3, i, r);
   }
+
+  // keep rest
+  memmove(sha3->buf->items, sha3->buf->items + i * r / 8, (sha3->buf->count -= i * r / 8));
 }
 
 void __sha3_squeeze(sha3 sha3, const int r) {
@@ -281,6 +216,17 @@ void __sha3_squeeze(sha3 sha3, const int r) {
   }
 }
 
+void __sha3_pad(__sha3_da* da, const int x) {
+  const int bit_len = da->count * 8 - 5;
+  const int j = __sha3_mod(-bit_len - 2, x);
+  const int p_len = (bit_len + j + 2) / 8;
+
+  if(da->capacity < p_len) da->items = realloc(da->items, (da->capacity *= 2));
+
+  da->items[p_len - 1] |= 0b10000000;
+  da->count = p_len;
+}
+
 const uint64_t* sha3_squeeze(sha3* sha3) {
   const int c = 2 * sha3->d;
   const int r = SHA3_B - c;
@@ -290,11 +236,13 @@ const uint64_t* sha3_squeeze(sha3* sha3) {
   memset(sha3->buf->items + sha3->buf->count, 0, sha3->buf->capacity - sha3->buf->count);
   __sha3_pad(sha3->buf, r);
 
-  while(sha3->buf->count >= r / 8) {
-    __sha3_sponge(*sha3, r);
-
-    memmove(sha3->buf->items, sha3->buf->items + r / 8, (sha3->buf->count -= r / 8));
+  int i;
+  for(i = 0; (i + 1) * r / 8 <= sha3->buf->count; i++) {
+    __sha3_sponge(*sha3, i, r);
   }
+
+  // keep rest
+  memmove(sha3->buf->items, sha3->buf->items + i * r / 8, (sha3->buf->count -= i * r / 8));
 
   __sha3_squeeze(*sha3, r);
 
